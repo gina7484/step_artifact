7dd2983d39c55cf732c2769a8052abc71d5f059d
Gina Sohn <gina7484@gmail.com>
2025-06-02 23:23:02 +0000

    basic setup for pyo3

--- compile_proto.sh
+++ compile_proto.sh
@@ -1,2 +1,8 @@
-cd stepir/proto
+cd step_perf_ir/proto
 protoc --python_out=../../src/proto/ graph.proto ops.proto datatype.proto func.proto
+cd ../../
+
+cd step-perf
+cargo build
+maturin develop
+cd ../
