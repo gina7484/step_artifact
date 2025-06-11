969558ed6fd26101b5054a6e12d8bdfcbdc98e6f
Gina Sohn <gina7484@gmail.com>
2025-06-11 01:04:32 +0000

    prefill input stationary without partition, reassemble shape matching

--- tests/test_expert.py
+++ tests/test_expert.py
@@ -82,7 +82,7 @@ def test_prefill_expert_mnk_mnk():
     step_graph: MultiDiGraph = MultiDiGraph()
 
     in_load = OffChipLoad(
-        underlying=input,  # [B, N, H] => [B, N // gate_up_tile_config.m, H // gate_up_tile_config.k]
+        underlying=input,  # [B, N, H] => [B, N // 1, H // 32]
         stride=(
             N // gate_up_tile_config.m * H // gate_up_tile_config.k,
             H // gate_up_tile_config.k,
@@ -96,20 +96,20 @@ def test_prefill_expert_mnk_mnk():
         tile_row=gate_up_tile_config.m,
         tile_col=gate_up_tile_config.k,
         par_dispatch=4,
-    )  # [1, B, N // gate_up_tile_config.m, H // gate_up_tile_config.k]
+    )  # [1, B, N, H // 32]
 
     buff_input = Bufferize(
         graph=step_graph,
         input=in_load,
         rank=1,
-    )  # [1, B, N // gate_up_tile_config.m]
+    )  # [1, B, N] | [H // 32]
 
     formatted_input = Streamify(
         graph=step_graph,
         input=buff_input,
         repeat_factor=[MLP_HID // gate_up_tile_config.n],
         rank=1,
-    )  # [1, B, N // gate_up_tile_config.m, MLP_HID // gate_up_tile_config.n, H // gate_up_tile_config.k]
+    )  # [1, B, N, MLP_HID // 32, H // 32]
 
     # W_gate
     w_gate_load = DynOffChipLoad(
@@ -124,22 +124,22 @@ def test_prefill_expert_mnk_mnk():
         tile_row=gate_up_tile_config.k,
         tile_col=gate_up_tile_config.n,
         par_dispatch=4,
-    )  # [1, B, N // gate_up_tile_config.m, MLP_HID // gate_up_tile_config.n, H // gate_up_tile_config.k]
+    )  # [1, B, N, MLP_HID // 32, H // 32]
 
     # W_up
-    # w_up = DynOffChipLoad(
-    #     graph=step_graph,
-    #     ref=buff_input,
-    #     underlying=w_up,  # [H, MLP_HID] => [H // gate_up_tile_config.k, MLP_HID // gate_up_tile_config.n]
-    #     stride=(1, H // gate_up_tile_config.k),
-    #     out_shape_tiled=(
-    #         MLP_HID // gate_up_tile_config.n,
-    #         H // gate_up_tile_config.k,
-    #     ),
-    #     tile_row=gate_up_tile_config.k,
-    #     tile_col=gate_up_tile_config.n,
-    #     par_dispatch=4,
-    # )  # [1, B, N // gate_up_tile_config.m, MLP_HID // gate_up_tile_config.n, H // gate_up_tile_config.k]
+    w_up_load = DynOffChipLoad(
+        graph=step_graph,
+        ref=buff_input,
+        underlying=w_up,  # [H, MLP_HID] => [H // gate_up_tile_config.k, MLP_HID // gate_up_tile_config.n]
+        stride=(1, MLP_HID // gate_up_tile_config.n),
+        out_shape_tiled=(
+            MLP_HID // gate_up_tile_config.n,
+            H // gate_up_tile_config.k,
+        ),
+        tile_row=gate_up_tile_config.k,
+        tile_col=gate_up_tile_config.n,
+        par_dispatch=4,
+    )  # [1, B, N, MLP_HID // 32, H // 32]
 
     # ---------- Computation ----------
     # Linear (Gate)
@@ -151,43 +151,78 @@ def test_prefill_expert_mnk_mnk():
         rank=1,
         write_back_mu=False,
         compute_bw=GATE_COMPUTE_BW,
-    )
-
-    print = OffChipStore(graph=step_graph, input=gate, par_dispatch=4)
-    # Linear (Up)
-    # up = BinaryMapAccum(
-    #     graph=step_graph,
-    #     in1=formatted_input,
-    #     in2=w_up,
-    #     fn=map_fn.Matmul(),
-    #     rank=1,
-    #     write_back_mu=False,
-    #     compute_bw=UP_COMPUTE_BW,
-    # )
+    )  # [1, B, N, MLP_HID // 32]
 
     # Activation (SiLU)
+    silu_gate = UnaryMap(
+        graph=step_graph,
+        input=gate,
+        fn=map_fn.Silu(),
+        write_back_mu=False,
+        compute_bw=ACT_FN_COMPUTE_BW,
+    )
+
+    # Linear (Up)
+    up = BinaryMapAccum(
+        graph=step_graph,
+        in1=formatted_input,
+        in2=w_up_load,
+        fn=map_fn.Matmul(),
+        rank=1,
+        write_back_mu=False,
+        compute_bw=UP_COMPUTE_BW,
+    )  # [1, B, N, MLP_HID // 32]
 
     # Elementwise Multiplication (Gate * Up)
+    gate_up = BinaryMap(
+        graph=step_graph,
+        in1=silu_gate,
+        in2=up,
+        fn=map_fn.Mul(),
+        write_back_mu=True,
+        compute_bw=MULT_COMPUTE_BW,
+    )  # [1, B, N, MLP_HID // 32]
 
     # Linear (Down)
     # bufferize 1D
-    # streamify (H // down_tile_config.n)
-    # BinaryMapAccum (1d)
+    buff_gate_up = Bufferize(graph=step_graph, input=gate_up, rank=1)  # [1, B, N]
+    formatted_gate_up = Streamify(
+        graph=step_graph,
+        input=buff_gate_up,
+        repeat_factor=[H // down_tile_config.n],
+        rank=1,
+    )  # [1, B, N, H // 32, MLP_HID // 32]
+
+    w_down_load = DynOffChipLoad(
+        graph=step_graph,
+        ref=buff_input,
+        underlying=w_down,  # [MLP_HID, H] => [MLP_HID // down_tile_config.k, H // down_tile_config.n]
+        stride=(1, H // down_tile_config.n),
+        out_shape_tiled=(
+            H // down_tile_config.n,
+            MLP_HID // down_tile_config.k,
+        ),
+        tile_row=down_tile_config.k,
+        tile_col=down_tile_config.n,
+        par_dispatch=4,
+    )  # [1, B, N, H // 32, MLP_HID // 32]
+
+    down = BinaryMapAccum(
+        graph=step_graph,
+        in1=formatted_gate_up,
+        in2=w_down_load,
+        fn=map_fn.Matmul(),
+        rank=1,
+        write_back_mu=True,
+        compute_bw=DOWN_COMPUTE_BW,
+    )  # [1, B, N, H // 32]
 
     # Output
-    # output_gate = OffChipStore(
-    #     graph=step_graph,
-    #     input=gate,
-    #     par_dispatch=4,
-    #     store_file_name="gate_output",
-    # )
-    # output_up = OffChipStore(
-    #     graph=step_graph,
-    #     input=up,
-    #     par_dispatch=4,
-    #     store_file_name="up_output",
-    # )
-
+    output_up = OffChipStore(
+        graph=step_graph,
+        input=down,
+        par_dispatch=4,
+    )
     # ================ Check whether the shapes match ================
 
     # print(f"Gold (gate) shape: {gold_gate.shape}")
@@ -199,17 +234,20 @@ def test_prefill_expert_mnk_mnk():
     # ================ Rewrite passes ================
     step_graph = infer_broadcast(step_graph)
 
+    print(f"Output shape: {output_up.get_untiled_shape()}")
+    print(f"{[B,N,H]}")
+
     # ================ Print the STeP Graph ================
-    OUTPUT_FILENAME = "linear_gate_up"
+    OUTPUT_FILENAME = "input_stationary"
     save_graph_format(step_graph, OUTPUT_FILENAME, ["png"])
 
     # ================ Simulate & Check the output ================
-    simulate(
-        step_graph,
-        False,  # logging
-        HBMConfig(64, 8, 2, 2, 1, 14),
-        "/home/ginasohn/step_tl/graph.pb",
-    )
+    # simulate(
+    #     step_graph,
+    #     False,  # logging
+    #     HBMConfig(64, 8, 2, 2, 1, 14),
+    #     "/home/ginasohn/step_tl/graph.pb",
+    # )
 
-    check_gold_tensor("output", gold_gate)
+    # check_gold_tensor("output", gold_gate)
     # check_gold_tensor("up_output", gold_up)
